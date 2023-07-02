daf93fab37a6609e3bedbbdb050b39d63037d27f
Marko-Pajic <pajic.marko84@gmail.com>
2023-07-02 02:19:04 +0200

    Fixed #3 made a little more generic approach to conditions and switched out "i" and "j"

--- Program.cs
+++ Program.cs
@@ -25,17 +25,19 @@ namespace SlotMachine
             int cashDeposit = Convert.ToInt32(Console.ReadLine());
             while (cashDeposit >= MINIMUM_WAGER)
             {
-                int[,] slotMachine = new int[GRID_ROW, GRID_COLUMN];
+                int[,] slotMachine = new int[,] { { 1, 3, 1 },
+                                                  { 2, 2, 2 },
+                                                  { 3, 4, 5 } };//[GRID_ROW, GRID_COLUMN];
 
-                for (int row = 0; row < slotMachine.GetLength(0); row++)
-                {
-                    for (int col = 0; col < slotMachine.GetLength(1); col++)
-                    {
-                        slotMachine[row, col] = rng.Next(1, UPPER_BOUND);
-                        Console.Write(" " + slotMachine[row, col] + " ");
-                    }
-                    Console.WriteLine();
-                }
+                //for (int row = 0; row < slotMachine.GetLength(0); row++)
+                //{
+                //    for (int col = 0; col < slotMachine.GetLength(1); col++)
+                //    {
+                //        slotMachine[row, col] = rng.Next(1, UPPER_BOUND);
+                //        Console.Write(" " + slotMachine[row, col] + " ");
+                //    }
+                //    Console.WriteLine();
+                //}
                 Console.WriteLine("How much is your wager:");
                 int wagerAmount = Convert.ToInt32(Console.ReadLine());
                 int allRowsandColsChecked = GRID_ROW + GRID_COLUMN;
@@ -44,22 +46,26 @@ namespace SlotMachine
                     int numberOfRowsToCheck = Math.Min(wagerAmount, GRID_ROW);
                     for (int row = 0; row < numberOfRowsToCheck; row++)
                     {
+                        bool allValuesAreEqual = true;
+                        int firstValueOfRLine = slotMachine[row, 0];
                         for (int col = 0; col < GRID_COLUMN; col++)
                         {
                             Console.Write(" " + slotMachine[row, col] + " ");
-                            if (slotMachine[row, col + col] == slotMachine[row, col - col] && slotMachine[row, col] == slotMachine[row, col + col])
+                            if (firstValueOfRLine != slotMachine[row, col])
                             {
-                                cashDeposit++;
+                                allValuesAreEqual = false;
+                                break;
                             }
                             Console.WriteLine();
                         }
-                        //            Console.WriteLine("YES");
-                        //        }
-                        //    }
+                        if (allValuesAreEqual)
+                        {
+                            cashDeposit++;
+                        }
                     }
-                    if (wagerAmount > GRID_COLUMN && wagerAmount <= allRowsandColsChecked)
+                    if (wagerAmount > GRID_ROW && wagerAmount <= allRowsandColsChecked)
                     {
-                        int numberOfColumnsToCheck = wagerAmount - GRID_COLUMN;
+                        int numberOfColumnsToCheck = wagerAmount - GRID_ROW;
                         for (int col = 0; col < numberOfColumnsToCheck; col++)
                         {
                             for (int row = 0; row < GRID_ROW; row++)
@@ -69,17 +75,10 @@ namespace SlotMachine
                             }
                             Console.WriteLine();
                         }
-
-                        //        if (slotMachine[0, j] == slotMachine[1, j] && slotMachine[1, j] == slotMachine[2, j])
-                        //        {
-                        //            Console.WriteLine("NO");
-                        //        }
-                        //    }
-                        //}
                         //if (amountForPlaying > UPPER_BOUND && amountForPlaying <= MAXIMUM_WAGER)
-                        {
+                        //{
 
-                        }
+                        //}
                     }
                 }
             }
